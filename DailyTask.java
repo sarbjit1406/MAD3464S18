8c5a8d577a266d3f38cfbba2167817fe4388eb0f
sarbjit1406 <39779037+sarbjit1406@users.noreply.github.com>
2018-06-01 16:18:22 -0400

    Add files via upload

---
+++ DailyTask.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author macstudent
+ */
+public class DailyTask {
+   public static void main(String[] args){
+        
+        Arithmetic operation1 = new Arithmetic() ;
+        operation1.num1 = 20;
+        System.out.println("num 1: " + operation1.num1);
+        operation1.addition(); 
+    
+}
